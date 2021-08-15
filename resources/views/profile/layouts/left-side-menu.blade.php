d040cd31587fd6182395892e01da76ee51f8d932
Пеливан Эдуард Владимирович <eduard.pelivan@resmi.kz>
2021-08-15 18:05:18 +0600

    fixes

--- resources/views/profile/layouts/left-side-menu.blade.php
+++ resources/views/profile/layouts/left-side-menu.blade.php
@@ -1,3 +1,31 @@
+<style>
+    @media only screen and (max-width: 600px) {
+        .leftMenu {
+            display: flex;
+            /*s*/
+        }
+        .userData {
+           margin-top: -20px !important;
+        }
+
+        .full-height {
+            max-height: 340px;
+            /*height: calc(100vh - 5%)*/
+        }
+        .userData h2 {
+            padding-top: 30px;
+        }
+
+        #left-side-block .userData {
+            margin: 5%;
+        }
+        .counterRight {
+            margin-left: 25%;
+        }
+
+    }
+</style>
+
 @push('styles')
     <link rel="stylesheet" type="text/css" href="{{ asset('css/left-side.css') }}">
 @endpush
@@ -6,7 +34,7 @@
         <h2 class="text-white">{{Auth::user()->last_name}} {{Auth::user()->first_name}}</h2>
         {{--<h3 class="heading">Скидка</h3>--}}
         {{--<p class="discount">5%</p>--}}
-        <div class="row">
+        <div class="row leftMenu">
             <div class="col-md-8 col-sm-8">
                 <ul class="menu_list">
                     <li><a class="{{(\Request::route()->getName() == 'profile-favorite') ? 'active' : ''}}"
@@ -19,7 +47,7 @@
                            href="{{route('profile-orders')}}">Мои заказы</a></li>
                 </ul>
                 <ul class="additional">
-                    {{--<li><a href="">Спецпредложения</a></li>--}}s
+                    {{--<li><a href="">Спецпредложения</a></li>--}}
                     <li><a class="{{(\Request::route()->getName() == 'profile') ? 'active' : ''}}"
                            href="{{route('profile')}}">Персональные данные</a></li>
                 </ul>
@@ -32,7 +60,7 @@
                     </form>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-4">
+            <div class="col-md-4 col-sm-4 counterRight">
                 <ul class="count-list text-right">
                     <li id="favorite_count">{{$favorite_count}}</li>
                     <li>{{$subscription_count}}</li>
