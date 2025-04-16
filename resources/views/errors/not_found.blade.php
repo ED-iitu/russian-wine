714daab25d8646c02941d8bdc43f125ff691dd18
pelivan <pelivan@kolesa.kz>
2025-04-16 10:52:16 +0500

    fixes

--- resources/views/errors/not_found.blade.php
+++ resources/views/errors/not_found.blade.php
@@ -1,11 +1,27 @@
-@extends('layouts.app') {{-- если используешь layout --}}
+@extends('layouts.app')
 
 @section('title', 'Страница не найдена')
 
 @section('content')
-    <div style="text-align: center; padding: 50px;">
-        <h1>404</h1>
-        <p>Упс! Страница не найдена.</p>
-        <a href="{{ route('wine_shop') }}">Вернуться в магазин</a>
+    <div style="
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        height: 80vh;
+        text-align: center;
+    ">
+        <h1 style="font-size: 6rem; margin-bottom: 1rem;">404</h1>
+        <p style="font-size: 1.5rem; margin-bottom: 2rem;">Упс! Страница не найдена.</p>
+        <a href="{{ route('wine_shop') }}" style="
+            padding: 0.75rem 1.5rem;
+            background-color: #a42134;
+            color: white;
+            border-radius: 5px;
+            text-decoration: none;
+            font-weight: bold;
+        ">
+            Вернуться в магазин
+        </a>
     </div>
 @endsection
