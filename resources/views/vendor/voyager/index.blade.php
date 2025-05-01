759def4012fa45e1608d2b70913b139be9ccacbe
pelivan <pelivan@kolesa.kz>
2025-05-02 04:04:00 +0500

    fixes

--- resources/views/vendor/voyager/index.blade.php
+++ resources/views/vendor/voyager/index.blade.php
@@ -1,24 +1,27 @@
 @extends('voyager::master')
 
 @section('content')
-    <div class="page-content">
-        <h1 class="page-title">Дашборд</h1>
+    <div class="container">
+        <div class="page-content">
+            <h1 class="page-title">Дашборд</h1>
 
-        <div class="row">
-            <!-- Панель с количеством заказов -->
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header bg-primary text-white">
-                        <h4 class="card-title">Кол-во заказов</h4>
-                    </div>
-                    <div class="card-body text-center">
-                        <h1 class="display-4">{{ $totalOrders }}</h1>
-                        <p>Всего заказов</p>
+            <div class="row">
+                <!-- Панель с количеством заказов -->
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header bg-primary text-white">
+                            <h4 class="card-title">Кол-во заказов</h4>
+                        </div>
+                        <div class="card-body text-center">
+                            <h1 class="display-4">{{ $totalOrders }}</h1>
+                            <p>Всего заказов</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
 
 @section('css')
