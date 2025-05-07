a7d8d2518f65b523aa6cf64cd0c0c1fb49857803
pelivan <pelivan@kolesa.kz>
2025-05-07 16:13:14 +0500

    fixes

---
+++ app/Http/Controllers/API/WineController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
+use App\Models\Color;
+use App\Models\GrapeSort;
+use App\Models\Region;
+use App\Models\Sugar;
+use App\Models\Wine;
+use App\Models\WineClass;
+use Illuminate\Http\Request;
+
+class WineController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Wine::query();
+
+        foreach ($request->all() as $field => $value) {
+            if (in_array($field, ['color', 'type', 'year', 'author'])) { // только разрешённые поля
+                $query->where($field, $value);
+            }
+        }
+
+        return response()->json($query->get());
+    }
+
+    public function filters()
+    {
+        $colors = Color::orderBy('title', 'ASC')->get();
+        $regions = Region::orderBy('title', 'ASC')->get();
+        $sugars = Sugar::orderBy('title', 'ASC')->get();
+        $sorts = GrapeSort::orderBy('title', 'ASC')->get();
+        $classes = WineClass::orderBy('title', 'ASC')->get();
+        $years = Wine::select('year')->where('year', '!=', null)->groupBy('year')->orderBy('year', 'DESC')->get();
+        $fortresses = Wine::select('fortress')->where('fortress', '!=', null)->groupBy('fortress')->orderBy('fortress', 'DESC')->get();
+
+        return response()->json([
+            'colors'     => $colors,
+            'regions'    => $regions,
+            'sugars'     => $sugars,
+            'sorts'      => $sorts,
+            'classes'    => $classes,
+            'years'      => $years,
+            'fortresses' => $fortresses,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $wine = Wine::findOrFail($id);
+
+        return response()->json($wine);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/wines', 'API\WineController@index');
+Route::get('/wines/filters', 'API\WineController@filters');
+Route::get('/wines/{id}', 'API\WineController@show');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
