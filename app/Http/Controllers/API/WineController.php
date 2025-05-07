324be399c1432b65fd7429daf955465cbbdfad8a
pelivan <pelivan@kolesa.kz>
2025-05-07 16:56:41 +0500

    fixes

--- app/Http/Controllers/API/WineController.php
+++ app/Http/Controllers/API/WineController.php
@@ -19,7 +19,7 @@ class WineController extends Controller
         $query->with(['color', 'region', 'sugar']);
 
         foreach ($request->all() as $field => $value) {
-            if (in_array($field, ['color', 'type', 'year', 'author'])) { // только разрешённые поля
+            if (in_array($field, ['color_id', 'sugar_id', 'year', 'region_id'])) { // только разрешённые поля
                 $query->where($field, $value);
             }
         }
