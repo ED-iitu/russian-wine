b3f43e20ea00cd19b1044ec267bd461e3a20769e
pelivan <pelivan@kolesa.kz>
2025-05-08 16:40:37 +0500

    fixes

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -9,17 +9,51 @@ class TelegramController extends Controller
 {
     public function webhook(Request $request)
     {
-        Log::info('TELEGRAM DATA: ' . json_encode($request->all()));
-
         $data = $request->all();
 
-        if (isset($data['message']['text']) && $data['message']['text'] === '/start') {
+        // Логирование входящего сообщения
+        Log::info('Telegram Data: ' . json_encode($data));
+
+        if (isset($data['message']['text'])) {
             $chatId = $data['message']['chat']['id'];
-            $this->sendMessage($chatId, 'Привет! Добро пожаловать в бота 🎉');
+            $text = $data['message']['text'];
+
+            // Обрабатываем команду /start
+            if ($text === '/start') {
+                $keyboard = [
+                    'inline_keyboard' => [
+                        [
+                            ['text' => 'Помощь', 'callback_data' => 'help'],
+                            ['text' => 'Контакты', 'callback_data' => 'contacts'],
+                        ],
+                        [
+                            ['text' => 'Посмотреть каталог вин', 'callback_data' => 'store'],
+                        ]
+                    ]
+                ];
+
+                $this->sendMessageWithKeyboard($chatId, 'Добро пожаловать в наш бот! Выберите одну из команд ниже:', $keyboard);
+            }
+
+            // Обрабатываем другие команды, например /help
+            elseif ($text === '/help') {
+                $this->sendMessage($chatId, 'Помощь: Используй команду /order для оформления заказа или /catalog для просмотра каталога.');
+            }
+
+            // Обрабатываем команду /order
+            elseif ($text === '/order') {
+                $this->sendMessage($chatId, 'Для оформления заказа перейдите по ссылке...');
+            }
+
+            // Обрабатываем команду /catalog
+            elseif ($text === '/catalog') {
+                $this->sendMessage($chatId, 'Вот наш каталог вин...');
+            }
         }
 
         return response('OK', 200);
     }
+
 
     private function sendMessage($chatId, $text)
     {
@@ -43,5 +77,28 @@ class TelegramController extends Controller
 
         Log::info('Telegram API Response: ' . $response);
     }
+
+    private function sendMessageWithKeyboard($chatId, $text, $keyboard)
+    {
+        $url = "https://api.telegram.org/bot" . '7472810776:AAEZls-YtfWyL0T9mnzQFXnukSAnOg-owoo' . "/sendMessage";
+
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $text,
+            'reply_markup' => json_encode($keyboard),
+        ];
+
+        $options = [
+            'http' => [
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+                'content' => http_build_query($data),
+            ],
+        ];
+
+        $context = stream_context_create($options);
+        file_get_contents($url, false, $context);
+    }
+
 
 }
