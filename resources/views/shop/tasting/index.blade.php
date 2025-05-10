4ca626aa39b467d047b89e1fbbbd2db3056287dd
pelivan <pelivan@kolesa.kz>
2025-05-10 14:43:51 +0500

    fixes

--- resources/views/shop/tasting/index.blade.php
+++ resources/views/shop/tasting/index.blade.php
@@ -161,7 +161,7 @@
                 <div class="row">
                     <div class="col-md-4 col-md-offset-4">
                         <h2 class="text-center">Остались вопросы?</h2>
-                        <p class="text-center">Свяжитесь с нами по указанным данным или отправьте сообщение</p>
+                        <p class="text-center">Свяжитесь с нами по указанным данным или отправьте сообщение
                     </div>
                     <div class="col-md-12 col-md-offset-1">
                         <div class="col-xs-12 col-sm-12 col-md-5 question_block">
