102cb4aaf135019287f16fc692a61506b2a38cae
pelivan <pelivan@kolesa.kz>
2025-08-15 03:13:30 +0500

    style fixes

--- resources/views/page/giftcards/index.blade.php
+++ resources/views/page/giftcards/index.blade.php
@@ -3,21 +3,7 @@
 @section('content')
     <div id="blur_cont">
         <div id="blur-cont" class="sety-category">
-            <div id="sety-banner">
-                <h1 class="forwc">Подарочные карты</h1>
-                <p>🎁 Подарочные карты «Русское Вино» — дарите вкус и эмоции!</p>
-            </div>
-            <div id="sety-section">
-                <div class="product_cont">
-                    <div class="product_info">
-                        <a href="">123</a>
-                        <span>5000 <b>п</b></span>
-                    </div>
-                    <a href="">
-                        <img alt="123" src="">
-                    </a>
-                </div>
-            </div>
+
         </div>
     </div>
 @endsection
