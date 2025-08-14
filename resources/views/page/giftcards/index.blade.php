405a93084e9c3439852f629d83cff880efea3c4e
pelivan <pelivan@kolesa.kz>
2025-08-15 03:06:28 +0500

    style fixes

--- resources/views/page/giftcards/index.blade.php
+++ resources/views/page/giftcards/index.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 @section('title', '🎁 Подарочные карты «Русское Вино» — дарите вкус и эмоции!')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <h1>🎁 Подарочные карты «Русское Вино» — дарите вкус и эмоции!</h1>
-        </div>
+    <div class="row subHeader">
+        <ul class="breadcrumb" id="breadcrumb">
+            <li><a href="{{route('home')}}">Главная</a></li>
+            <li><a href="{{route('wine_shop')}}">Подарочные карты</a></li>
+        </ul>
+        <h1 class="pageHeading">🎁 Подарочные карты «Русское Вино» — дарите вкус и эмоции!</h1>
+        <p class="pageDesc">Идеальный подарок для ценителей вина — от 5 000 до 20 000 рублей.</p>
     </div>
 @endsection
