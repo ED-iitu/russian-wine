ee3348b84fef31a465d5f77d25caa189c972a109
pelivan <pelivan@kolesa.kz>
2025-08-18 22:30:17 +0500

    style fixes

--- resources/views/shop/wine/show.blade.php
+++ resources/views/shop/wine/show.blade.php
@@ -203,7 +203,7 @@
                 bottom: 0;
                 left: 38px; /* для левой колонки */
                 width: 100%; /* делаем линию длиннее */
-                height: 1.3px;
+                height: 1.1px;
                 background: #e0e0e0;
             }
 
@@ -213,7 +213,7 @@
                 bottom: 0;
                 left: -40px; /* для левой колонки */
                 width: 110%; /* делаем линию длиннее */
-                height: 1.3px;
+                height: 1.1px;
                 background: #e0e0e0;
             }
         }
