6301d7056ae17010f30a64af0e3fb87a025e6abc
pelivan <pelivan@kolesa.kz>
2025-08-19 21:41:52 +0500

    style fixes

--- resources/views/page/giftcards/index.blade.php
+++ resources/views/page/giftcards/index.blade.php
@@ -383,4 +383,15 @@
             }
         };
     </script>
+
+    <script>
+        const faqItems = document.querySelectorAll('.faq-item');
+
+        faqItems.forEach(item => {
+            const question = item.querySelector('.faq-question');
+            question.addEventListener('click', () => {
+                item.classList.toggle('active');
+            });
+        });
+    </script>
 @endsection
