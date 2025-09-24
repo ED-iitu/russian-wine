110686fc718c82e47cc4b326235e23345d933c5d
pelivan <pelivan@kolesa.kz>
2025-09-24 21:56:51 +0500

    style fixes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -108,7 +108,7 @@
 {{--                    @endif--}}
 {{--                </div>--}}
                 <!-- Кнопка поиска -->
-                <div id="navSearch" class="desktopHidden mobile-search">
+                <div class="desktopHidden mobile-search" id="navSearch">
                     <a onclick="$('#myOverlay').addClass('open', 1000); $('#searchResult').show();
                 $('#white-logo').hide(); $('.overlay-results').hide();  $('#black-logo').show()"
                        id="searchStart">
