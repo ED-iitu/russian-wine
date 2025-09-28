893224d733eb458a32d43111c624e8300f9203df
pelivan <pelivan@kolesa.kz>
2025-09-28 18:52:30 +0500

    style fixes

--- resources/views/shop/wine/wine-list.blade.php
+++ resources/views/shop/wine/wine-list.blade.php
@@ -53,7 +53,7 @@
 {{--                                        onclick="cart_add('{{$wine->id}}', 1, 'wine');$(this).addClass('active')">--}}
 {{--                                    <span>В корзину</span></button>--}}
                                 <button id="button-carts" class="cart-btn-{{$wine->id}}"
-                                        onclick="cart_button_click('{{$wine->id}}', 1, 'wine');$(this).addClass('active')">
+                                        onclick="cart_add('{{$wine->id}}', 1, 'wine');$(this).addClass('active')">
                                     <span>В корзину</span></button>
                             </div>
                         </div>
