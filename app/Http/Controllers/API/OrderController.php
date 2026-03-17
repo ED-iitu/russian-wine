7ae5b788af125b910228e972e0e67cb226127720
pelivan <pelivan@kolesa.kz>
2026-03-17 14:16:39 +0500

    fixes

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\Mail\IndexController as SendMail;
 use App\Models\Order;
-use App\Models\User;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
